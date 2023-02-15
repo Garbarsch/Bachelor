6d79a326d481933585a108ad92a2879aa396e546
Carl Bruun <carbr@itu.dk>
2023-02-15 15:07:16 +0100

    Fix pubspec.yaml error for windows Setup bloc architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,107 @@
 //import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:github_client/bloc/home_page_bloc.dart';
+import 'package:github_client/models/municipality_model.dart';
 import 'package:syncfusion_flutter_maps/maps.dart';
 
 void main() {
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => HomePageBloc(),
+          )
+        ],
+        child:  MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "Danish Municipalities",
+          home: MyHomePage(),
+        ),
     );
   }
 }
+
+class MyHomePage extends StatelessWidget{
+
+  MyHomePage({
+    super.key,
+    //required this.shapeSource, required this.mapData,
+  });
+
+  final List<Municipality> mapData =  Municipality(name: 'h').getList;
+
+  final MapShapeSource shapeSource =
+      MapShapeSource.asset('assets/municipalitiesDK.json',
+      shapeDataField: 'label_dk',
+      dataCount: defaultList.length,
+      primaryValueMapper: (int index) => defaultList[index].name);
+
+  //Have to create a static member to initialize shapeSource.
+  static List<Municipality> get defaultList => Municipality(name: 'h').getList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+            padding: EdgeInsets.fromLTRB(0,0, 0,0),
+            child: Column(
+              children: [ Container(height:35, width: MediaQuery.of(context).size.width ,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      border: Border.all(width: 45.0, color: Colors.black),
+
+                    ),
+                    child: Text('  Danish Municipalities',
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25, height:1.5)),
+
+                  )),Container(height: 100), Container(height: 300,
+                  child: SfMaps(
+                    layers: [
+                      MapShapeLayer(source: shapeSource,
+                        showDataLabels: false,
+                        dataLabelSettings: MapDataLabelSettings(
+                            textStyle: TextStyle(
+                                color: Colors.transparent)),
+                        color: Colors.white,
+                        shapeTooltipBuilder: (BuildContext context, int index){
+                          return Padding(padding: EdgeInsets.all(7),
+                              child: Text(mapData[index].name,
+                                style: TextStyle(color: Colors.white),
+                              )
+                          );
+                        },
+                        tooltipSettings:
+                        MapTooltipSettings(color: Colors.blue),
+
+                      )
+                    ],
+                  )),
+
+              ],)));
+  }
+  
+
+}
+
+/*
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -217,3 +288,4 @@ class MapModel{
   late String kommune;
 }
 
+*/
