3946bf8454b1bfe369ad398c0cdf97786bc3a349
Carl Bruun <carbr@itu.dk>
2023-02-15 15:12:27 +0100

    Moved home page screen to UI repo

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:github_client/bloc/home_page_bloc.dart';
 import 'package:github_client/models/municipality_model.dart';
 import 'package:syncfusion_flutter_maps/maps.dart';
 
+import 'ui/home_screen.dart';
+
 void main() {
 
   runApp(const MyApp());
@@ -36,69 +38,6 @@ class MyApp extends StatelessWidget {
         ),
     );
   }
-}
-
-class MyHomePage extends StatelessWidget{
-
-  MyHomePage({
-    super.key,
-    //required this.shapeSource, required this.mapData,
-  });
-
-  final List<Municipality> mapData =  Municipality(name: 'h').getList;
-
-  final MapShapeSource shapeSource =
-      MapShapeSource.asset('assets/municipalitiesDK.json',
-      shapeDataField: 'label_dk',
-      dataCount: defaultList.length,
-      primaryValueMapper: (int index) => defaultList[index].name);
-
-  //Have to create a static member to initialize shapeSource.
-  static List<Municipality> get defaultList => Municipality(name: 'h').getList;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Padding(
-            padding: EdgeInsets.fromLTRB(0,0, 0,0),
-            child: Column(
-              children: [ Container(height:35, width: MediaQuery.of(context).size.width ,
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      border: Border.all(width: 45.0, color: Colors.black),
-
-                    ),
-                    child: Text('  Danish Municipalities',
-                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25, height:1.5)),
-
-                  )),Container(height: 100), Container(height: 300,
-                  child: SfMaps(
-                    layers: [
-                      MapShapeLayer(source: shapeSource,
-                        showDataLabels: false,
-                        dataLabelSettings: MapDataLabelSettings(
-                            textStyle: TextStyle(
-                                color: Colors.transparent)),
-                        color: Colors.white,
-                        shapeTooltipBuilder: (BuildContext context, int index){
-                          return Padding(padding: EdgeInsets.all(7),
-                              child: Text(mapData[index].name,
-                                style: TextStyle(color: Colors.white),
-                              )
-                          );
-                        },
-                        tooltipSettings:
-                        MapTooltipSettings(color: Colors.blue),
-
-                      )
-                    ],
-                  )),
-
-              ],)));
-  }
-  
-
 }
 
 /*
---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:github_client/models/municipality_model.dart';
+import 'package:syncfusion_flutter_maps/maps.dart';
+
+
+class MyHomePage extends StatelessWidget{
+
+  MyHomePage({
+    super.key,
+    //required this.shapeSource, required this.mapData,
+  });
+
+  final List<Municipality> mapData =  Municipality(name: 'h').getList;
+
+  final MapShapeSource shapeSource =
+  MapShapeSource.asset('assets/municipalitiesDK.json',
+      shapeDataField: 'label_dk',
+      dataCount: defaultList.length,
+      primaryValueMapper: (int index) => defaultList[index].name);
+
+  //Have to create a static member to initialize shapeSource.
+  static List<Municipality> get defaultList => Municipality(name: 'h').getList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+            padding: EdgeInsets.fromLTRB(0,0, 0,0),
+            child: Column(
+              children: [ Container(height:35, width: MediaQuery.of(context).size.width ,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      border: Border.all(width: 45.0, color: Colors.black),
+
+                    ),
+                    child: Text('  Danish Municipalities',
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25, height:1.5)),
+
+                  )),Container(height: 100), Container(height: 300,
+                  child: SfMaps(
+                    layers: [
+                      MapShapeLayer(source: shapeSource,
+                        showDataLabels: false,
+                        dataLabelSettings: MapDataLabelSettings(
+                            textStyle: TextStyle(
+                                color: Colors.transparent)),
+                        color: Colors.white,
+                        shapeTooltipBuilder: (BuildContext context, int index){
+                          return Padding(padding: EdgeInsets.all(7),
+                              child: Text(mapData[index].name,
+                                style: TextStyle(color: Colors.white),
+                              )
+                          );
+                        },
+                        tooltipSettings:
+                        MapTooltipSettings(color: Colors.blue),
+
+                      )
+                    ],
+                  )),
+
+              ],)));
+  }
+}
