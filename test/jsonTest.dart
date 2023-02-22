1a7a8b4a9b6cbe76dd4b7b58e856b17280dae2d2
Carl Bruun <carbr@itu.dk>
2023-02-22 15:39:41 +0100

    more json repo test

--- test/jsonTest.dart
+++ test/jsonTest.dart
@@ -11,33 +11,68 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:github_client/data/jsonRepository.dart';
 
 import 'package:github_client/main.dart';
+import 'package:github_client/models/node.dart';
+import 'package:tuple/tuple.dart';
 
 void main() async{
 
   WidgetsFlutterBinding.ensureInitialized(); //small fix for some async shit
 
   final repo = jsonRepository();
-  var scc = await repo.loadJsonData();
-  /*if(scc == "success"){
-    repo.printAllNodes();
-  }*/
+  List<Node> nodes = [];
+  //seed data
+  var node1 = Node(id: 1, lon: 1, lat: 1, isAmenity: true, tags: {'amenity':'cafe'});
+  nodes.add(node1);
+  var node2 = Node(id: 2, lon: 2, lat: 2, isAmenity: true, tags: {'amenity':'restaurant'});
+  nodes.add(node2);
+  var node3 = Node(id: 3, lon: 3, lat: 3, isAmenity: true, tags: {'amenity':'college'});
+  nodes.add(node3);
+  var node4 = Node(id: 4, lon: 4, lat: 4, isAmenity: true, tags: {'amenity':'university'});
+  nodes.add(node4);
 
-  print("testing Cafe: " );
+  repo.nodes = nodes;
+
+
+
+
+  await testLoadJSON();
+
+
   testLatLonFromCafe(repo);
 
 
-  final x = 2;
-  final y = 2;
+  testLatLonFromHigherEducation(repo);
+}
 
-  expect(x, equals(y));
+Future<void> testLoadJSON() async{
+  var repo = jsonRepository();
+  var scc = await repo.loadJsonData();
+  if(scc == "success"){
+    print("load JSON test: Success \n");
+  }else{
+    print("load JSON test: Fail \n");
+  }
 }
 
 void testLatLonFromCafe(jsonRepository repo){
   var values = repo.getCafesCoords();
-  if(values.isNotEmpty){
-    print("Café test: Success");
-  }else{
-    print("Café test: Fail");
-  }
 
+  if(values.length == 1 && values.contains(Tuple2(1,1))){
+    print("Café test: Success \n");
+  }else{
+    print("Café test: Fail \n");
+    print(values);
+  }
+}
+
+
+void testLatLonFromHigherEducation(jsonRepository repo){
+  var values = repo.getHigherEducationCoords();
+
+  if(values.length == 2 && values.contains(Tuple2(3, 3))){
+    print("Higher education test: Success \n");
+  }else{
+    print("Higher education test: Fail \n");
+    print(values);
+  }
 }
